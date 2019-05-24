e09bdc5bc8897d83cfafd70a21285bb166983f8d
Andre Luis Florencio da Silva <andre.luis.florencio@gmail.com>
2019-05-24 18:08:00 -0300

    Aula 4 - Lendo arquivo com FS em Buffer

---
+++ util/fileReader.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+fs.readFile('imagem.jpg', function (err, buffer) {
+    console.log('arquivo lido');
+
+    fs.writeFile('imagem2.jpg', buffer, function(err){
+        console.log('arquivo escrito');
+        
+    })
+    
+});
