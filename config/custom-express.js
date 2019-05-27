9d09c8e3c3a99b82232d2447ef36954a885012a1
Andre Luis Florencio da Silva <andre.luis.florencio@gmail.com>
2019-05-27 18:56:03 -0300

    Aula 06 - Incluindo o morgam para integrar com express

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');;
+const expressValidator = require('express-validator');
+const morgan = require('morgan');
+const logger = require('../servicos/logger.js')
 
 module.exports = () =>{
 
     const app = express();
+
+    app.use(morgan('common', {
+        stream:{
+            write: function(mensagem){
+                logger.info(mensagem);
+            }
+        }
+    }));
 
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
